00e669a7b4abfb634ec8adb7a74321a93075c755
Xeptias <bsegoviasmr1b@gmail.com>
2019-12-11 04:06:44 +0100

    ejemplos de java

---
+++ src/Proyecto01/Ejemplo01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto01;
+import java.util.Scanner;
+/**
+ *
+ * @author blas
+ */
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        Scanner obtenerNumero = new Scanner(System.in);
+        int numero,i,j;
+ 
+        System.out.print("indica la tabla de multiplicar: ");
+        numero = obtenerNumero.nextInt();
+ 
+        for(i = 1; i<=numero; i++)
+        {
+            for(j = 1; j <= 12; j++)
+            {
+                System.out.println(i + " X " + j + " = " + i*j);
+            }
+            System.out.println();
+        }
+    }
+}
